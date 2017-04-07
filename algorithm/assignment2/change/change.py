9664c3d9de9dcdb15ccbbefb606fe8a64f0e508c
brezhang <bre.zhang@gmail.com>
2017-04-06 23:42:37 -0400

    adding changes

--- algorithm/assignment2/change/change.py
+++ algorithm/assignment2/change/change.py
@@ -2,6 +2,15 @@
 import sys
 
 def get_change(m):
+	coins=[10,5,1]
+	if w==0:
+		return count
+	w=m
+	while w!=0:
+	for i in coins:
+		if w>=i:
+			w=w-i
+			count=count+1
     #write your code here
     return m
 
