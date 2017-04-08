24dbaa441272b92f132bae140d5beea660b7b664
brezhang <bre.zhang@gmail.com>
2017-04-08 12:47:08 -0400

    correct answer

--- algorithm/assignment2/fractional_knapsack/fractional_knapsack.py
+++ algorithm/assignment2/fractional_knapsack/fractional_knapsack.py
@@ -1,8 +1,37 @@
-# Uses python3
+# Uses python2
 import sys
 
 def get_optimal_value(capacity, weights, values):
-    value = 0.
+    ratio=[]
+    for i in range(len(weights)):
+        ratio.append(values[i]*1.0/weights[i]*1.0)
+    value=0
+    mid=0
+    loc=int
+    ratio_2=list(ratio)
+    ratio_1=[]
+    order=[i for i in range(len(ratio))]
+    while ratio_1!=ratio_2:
+        ratio_1=list(ratio_2)
+        for i in range(len(ratio_2)-1):
+            if ratio_2[i]>ratio_2[i+1]:
+                mid=ratio_2[i]
+                loc=order[i]
+                ratio_2[i]=ratio_2[i+1]
+                order[i]=order[i+1]
+                ratio_2[i+1]=mid
+                order[i+1]=loc
+    #print order
+    #print ratio_2 ,ratio_1,order,ratio
+    for i in reversed(order):
+        #print i
+        if capacity>=weights[i]:
+            capacity=capacity-weights[i]
+            value=value+values[i]
+        else:
+            value=value+ min(capacity,weights[i])*ratio[i]
+            break
+
     # write your code here
 
     return value
