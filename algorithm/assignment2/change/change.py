d30568e7cdf4cb1bb6d8772419a6913cea72f4ad
brezhang <bre.zhang@gmail.com>
2017-04-07 20:15:21 -0400

    -update

--- algorithm/assignment2/change/change.py
+++ algorithm/assignment2/change/change.py
@@ -1,19 +1,18 @@
-# Uses python3
+# Uses python2
 import sys
 
 def get_change(m):
 	coins=[10,5,1]
-	if w==0:
-		return count
 	w=m
+	count=0
 	while w!=0:
-	for i in coins:
-		if w>=i:
-			w=w-i
-			count=count+1
-    #write your code here
-    return m
+		for i in coins:
+			if w>=i:
+				w=w-i
+				count=count+1
+
+	return count
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+    m = int(sys.stdin.readline())
+    print get_change(m)
